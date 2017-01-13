dff210a35e695d28c3cd32b9097aab13a96f46b2
Jonas Peji <Jonas Peji>
2017-01-12 20:08:20 -0800

    enums

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -14,10 +14,13 @@ UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
-private:
-	ATank* GetControlledTank() const;
 
+
+protected:
+	UFUNCTION(BlueprintCallable, Category = "Setup")
+		ATank* GetControlledTank() const;
+
+private:
 	virtual void BeginPlay() override;
 
 	virtual void Tick( float DeltaTime ) override;
